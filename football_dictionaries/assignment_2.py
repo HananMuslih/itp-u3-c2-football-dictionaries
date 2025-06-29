459daa35ce5c706cc55cbd622458c38ecdf3af70
HananMuslih <hanan.muslih11@gmail.com>
2025-06-29 16:37:57 +0300

    Add full solution for Football Dictionaries project
    - Assignment 1: Converted list of player info into dictionary format
    - Assignment 2: Grouped players by position
    - Assignment 3: Grouped players by country and position

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,40 @@
+from pprint import pprint
+
+
 def players_by_position(squads_list):
-    pass
+    grouped = {}
+
+    for player in squads_list:
+        number = player[0]
+        position = player[1]
+        name = player[2]
+        date_of_birth = player[3]
+        caps = player[4]
+        club = player[5]
+        country = player[6]
+        club_country = player[7]
+        tournament_year = player[8]
+
+        player_dict = {
+            "number": number,
+            "position": position,
+            "name": name,
+            "date_of_birth": date_of_birth,
+            "caps": caps,
+            "club": club,
+            "country": country,
+            "club_country": club_country,
+            "tournament_year": tournament_year
+        }
+
+         if position not in grouped:
+            grouped[position] = []
+
+        grouped[position].append(player_dict)
+
+    return grouped
+
+
+
+result = players_by_position(SQUADS_DATA)
+pprint(result)
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,44 @@
+from pprint import pprint
+
+
 def players_by_country_and_position(squads_list):
-    pass
+     grouped = {}
+
+    for player in squads_list:
+        number = player[0]
+        position = player[1]
+        name = player[2]
+        date_of_birth = player[3]
+        caps = player[4]
+        club = player[5]
+        country = player[6]
+        club_country = player[7]
+        tournament_year = player[8]
+
+        player_dict = {
+            "number": number,
+            "position": position,
+            "name": name,
+            "date_of_birth": date_of_birth,
+            "caps": caps,
+            "club": club,
+            "country": country,
+            "club_country": club_country,
+            "tournament_year": tournament_year
+        }
+
+        if country not in grouped:
+            grouped[country] = {}
+
+        if position not in grouped[country]:
+            grouped[country][position] = []
+
+        grouped[country][position].append(player_dict)
+
+    return grouped
+
+
+result = players_by_country_and_position(SQUADS_DATA)
+pprint(result)
+
+    
